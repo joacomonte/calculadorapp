29b7390987687ea1aa759ceea1e7465388cbf663
Joaco Montesano <montesano1994@gmail.com>
2023-11-12 17:38:11 -0300

    checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 import "katex/dist/katex.min.css";
 import { evaluate } from "mathjs";
 import { InlineMath } from "react-katex";
+import { ComputeEngine } from "@cortex-js/compute-engine";
 
 export default function Home() {
   const [formula, setFormula] = useState("");
   const [result, setResult] = useState("");
   const [activeButton, setActiveButton] = useState(null);
   const [display, setDisplay] = useState<any>("");
+
+  const ce: any = new ComputeEngine();
 
   const handleButtonClick = (value: string) => {
     if (value === "=") {
@@ -35,12 +38,30 @@ export default function Home() {
     }
   };
 
+  const convertToLatex = (expression: string) => {
+    // Parse the expression using Compute Engine
+    const parsed = ce.parse(expression);
+
+    // Convert the parsed expression to a LaTeX string
+    let output = ce.serialize(parsed, "latex");
+    console.log(output);
+
+    // Check if the output contains 'error'
+    if (output.includes("error")) {
+      // Return the original expression if there's an error
+      return expression;
+    } else {
+      // Return the converted LaTeX output if no error
+      return output;
+    }
+  };
+
   return (
     <div className="max-w-xs mx-auto mt-10">
       <div className="bg-gray-800 text-white p-5 rounded-t-md">
         {/* Display Area for LaTeX Formula */}
         <div className="text-right text-2xl mb-4 min-h-[34px]">
-          <InlineMath math={display} />
+          <InlineMath math={convertToLatex(display)} />
         </div>
         {/* Display result */}
         <div className="text-right text-3xl min-h-[36px]">{result}</div>
